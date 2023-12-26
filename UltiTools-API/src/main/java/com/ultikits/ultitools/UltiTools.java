034d74b34d0984eb106f69d23800b595760b0cf4
wisdommen <41609381+wisdommen@users.noreply.github.com>
2023-12-26 23:32:45 +1100

    2023/12/26更新 API版本6.0.2 [UltiTools-API] 1.修复了插件启动找不到JSON依赖的问题

--- UltiTools-API/src/main/java/com/ultikits/ultitools/UltiTools.java
+++ UltiTools-API/src/main/java/com/ultikits/ultitools/UltiTools.java
@@ -279,22 +279,4 @@ public final class UltiTools extends JavaPlugin implements Localized {
         }
         return config;
     }
-
-    /**
-     * get UltiTools UUID
-     *
-     * @return UUID
-     * @throws IOException if an I/O error occurs
-     */
-    public static String getUltiToolsUUID() throws IOException {
-        File dataFile = new File(UltiTools.getInstance().getDataFolder(), "data.json");
-        JSON json = new cn.hutool.json.JSONObject();
-        if (dataFile.exists()) {
-            json = JSONUtil.readJSON(dataFile, StandardCharsets.UTF_8);
-        } else {
-            json.putByPath("uuid", IdUtil.simpleUUID());
-            json.write(new FileWriter(dataFile));
-        }
-        return json.getByPath("uuid").toString();
-    }
 }
