a212d87158572f34f1b4f8eb6dac99336c978069
qianmo <67282958@qq.com>
2024-01-16 10:32:21 +0800

    24/1/16 更新 版本 6.0.5
    1. feat(API) 添加了临时监听事件的 API

---
+++ UltiTools-API/src/main/java/com/ultikits/ultitools/interfaces/impl/PlayerTempListener.java
@@ -0,0 +1,55 @@
+package com.ultikits.ultitools.interfaces.impl;
+
+import com.ultikits.ultitools.UltiTools;
+import com.ultikits.ultitools.interfaces.TempEventHandler;
+import com.ultikits.ultitools.interfaces.TempListener;
+import lombok.*;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerEvent;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class PlayerTempListener<E extends PlayerEvent> implements TempListener {
+    private Class<E> eventClass;
+    private EventPriority priority = EventPriority.NORMAL;
+    private TempEventHandler<E> eventHandler;
+    private Player player;
+
+    public PlayerTempListener(Class<E> eventClass, TempEventHandler<E> eventHandler) {
+        this.eventClass = eventClass;
+        this.eventHandler = eventHandler;
+    }
+
+    public PlayerTempListener(Class<E> eventClass, TempEventHandler<E> eventHandler, Player player) {
+        this.eventClass = eventClass;
+        this.eventHandler = eventHandler;
+        this.player = player;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void register() {
+        Bukkit.getServer().getPluginManager().registerEvent(eventClass, this, priority,
+                (ignored, event) -> {
+                    try {
+                        if (player == null) {
+                            if (eventHandler.handle((E) event)) {
+                                unregister();
+                            }
+                        } else if (((E) event).getPlayer().equals(player)) {
+                            if (eventHandler.handle((E) event)) {
+                                unregister();
+                            }
+                        }
+                    } catch (ClassCastException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                UltiTools.getInstance()
+        );
+    }
+}
