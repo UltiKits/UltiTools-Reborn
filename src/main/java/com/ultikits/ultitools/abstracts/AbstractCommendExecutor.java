36629534e043248b88c8697e31be6b259a75fb89
haishen668 <2821396723@qq.com>
2024-07-28 16:32:09 +0800

    2024/7/28 更新 版本6.1.2 添加了参数错误提示功能及权限判断功能

--- src/main/java/com/ultikits/ultitools/abstracts/AbstractCommendExecutor.java
+++ src/main/java/com/ultikits/ultitools/abstracts/AbstractCommendExecutor.java
@@ -313,7 +313,7 @@ public abstract class AbstractCommendExecutor implements TabExecutor {
         if (permission.isEmpty() || sender.hasPermission(permission)) {
             return true;
         }
-        sender.sendMessage(UltiTools.getInstance().i18n("§7执行此命令需要 §f"+ permission +" §7权限"));
+        sender.sendMessage(ChatColor.GRAY + "执行此命令需要 " + ChatColor.WHITE + permission + ChatColor.GRAY + " 权限");
         return false;
     }
 
@@ -339,7 +339,7 @@ public abstract class AbstractCommendExecutor implements TabExecutor {
         if (sender.hasPermission(permission)) {
             return true;
         }
-        sender.sendMessage(UltiTools.getInstance().i18n("§7执行此命令需要 §f"+ permission +" §7权限"));
+        sender.sendMessage(ChatColor.GRAY + "执行此命令需要 " + ChatColor.WHITE + permission + ChatColor.GRAY + " 权限");
         return false;
     }
 
@@ -976,7 +976,7 @@ public abstract class AbstractCommendExecutor implements TabExecutor {
                             commandArgsStr += ("§7" + args[j] + " ");
                         }
                         // 告知错误位置
-                        commandSender.sendMessage("§c参数错误: §7/" + command.getName() + commandArgsStr + "§c§n" + args[min] + "§r §c<--§o[此处错误]");
+                        commandSender.sendMessage(ChatColor.RED + "参数错误: " + ChatColor.GRAY + "/" + command.getName() + commandArgsStr + ChatColor.RED + ChatColor.UNDERLINE + args[min] + ChatColor.RESET + " " + ChatColor.RED + "<--" + ChatColor.ITALIC + "[此处错误]");
                         break;
                     }
                 }
@@ -994,10 +994,10 @@ public abstract class AbstractCommendExecutor implements TabExecutor {
                 }
                 missingParameters = missingParameters.trim();
                 // 告知缺少参数的位置
-                commandSender.sendMessage("§c缺少参数: §7/" + command.getName() + commandArgsStr + "§c§n" + missingParameters + "§r §c<--§o[缺少部分]");
+                commandSender.sendMessage(ChatColor.RED + "缺少参数: " + ChatColor.GRAY + "/" + command.getName() + commandArgsStr + ChatColor.RED + ChatColor.UNDERLINE + missingParameters + ChatColor.RESET + " " + ChatColor.RED + "<--" + ChatColor.ITALIC + "[缺少部分]");
             }
             // 提示正确用法
-            commandSender.sendMessage("§e正确用法: §7/" + command.getName() + " " + format);
+            commandSender.sendMessage(ChatColor.YELLOW + "正确用法: " + ChatColor.GRAY + "/" + command.getName() + " " + format);
             return false;
         }
         return true;
